2d1399be2a357886113277b4a63bc7ad5dbfc346
ban-mazin <ban.qaddoori89@gmail.com>
2021-03-16 05:09:31 +0000

    use the EJS templating engine to render web pages

--- express_server.js
+++ express_server.js
@@ -2,6 +2,15 @@ const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
 
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);  // Log the POST request body to the console
+  res.send("Ok");         // Respond with 'Ok' (we will replace this)
+});
+
+
 app.set("view engine", "ejs");
 
 
@@ -9,6 +18,7 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
 
 app.get("/", (req, res) => {
   res.send("Hello!");
@@ -23,6 +33,10 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get("/urls/:shortURL", (req, res) => {
   const longUrl = urlDatabase[req.params.shortURL];
   console.log(longUrl);
@@ -31,11 +45,20 @@ app.get("/urls/:shortURL", (req, res) => {
 });
 
 
+app.get("/u/:shortURL", (req, res) => {
+  const longUrl = urlDatabase[req.params.shortURL];
+  res.redirect(longUrl);
+});
+
+
 
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+
 app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 });
+
+
